d6da8de6fce8f1cf97ce588080cec588b634a6bb
hatem ben tayeb <hatemtayeb2@gmail.com>
2026-01-25 22:04:19 +0100

    ✨ add git view navigation

---
+++ plugins/git/git_view_nav.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"omo/pkg/ui"
+)
+
+const (
+	viewRoot     = "git"
+	viewRepos    = "repos"
+	viewStatus   = "status"
+	viewCommits  = "commits"
+	viewBranches = "branches"
+	viewRemotes  = "remotes"
+	viewStash    = "stash"
+	viewTags     = "tags"
+	viewDiff     = "diff"
+)
+
+func (gv *GitView) currentCores() *ui.Cores {
+	switch gv.currentViewName {
+	case viewStatus:
+		return gv.statusView
+	case viewCommits:
+		return gv.commitsView
+	case viewBranches:
+		return gv.branchesView
+	case viewRemotes:
+		return gv.remotesView
+	case viewStash:
+		return gv.stashView
+	case viewTags:
+		return gv.tagsView
+	default:
+		return gv.reposView
+	}
+}
+
+func (gv *GitView) setViewStack(cores *ui.Cores, viewName string) {
+	if cores == nil {
+		return
+	}
+
+	stack := []string{viewRoot, viewRepos}
+	if viewName != viewRepos {
+		stack = append(stack, viewName)
+	}
+	cores.SetViewStack(stack)
+}
+
+func (gv *GitView) switchToView(viewName string) {
+	pageName := "git-" + viewName
+	gv.currentViewName = viewName
+	gv.viewPages.SwitchToPage(pageName)
+
+	gv.setViewStack(gv.currentCores(), viewName)
+	gv.refresh()
+	current := gv.currentCores()
+	if current != nil {
+		gv.app.SetFocus(current.GetTable())
+	}
+}
+
+func (gv *GitView) showRepos() {
+	gv.switchToView(viewRepos)
+}
+
+func (gv *GitView) showStatus() {
+	gv.switchToView(viewStatus)
+}
+
+func (gv *GitView) showCommits() {
+	gv.switchToView(viewCommits)
+}
+
+func (gv *GitView) showBranches() {
+	gv.switchToView(viewBranches)
+}
+
+func (gv *GitView) showRemotes() {
+	gv.switchToView(viewRemotes)
+}
+
+func (gv *GitView) showStash() {
+	gv.switchToView(viewStash)
+}
+
+func (gv *GitView) showTags() {
+	gv.switchToView(viewTags)
+}
